a744e9b692bf7bf0afc5e9c293b89a1268001f73
HOT-Media <jeremy@hotmediaglobalconnect.com>
2018-06-13 15:13:46 -0500

    Add Easy 1, exercise 1

---
+++ small_problems/easy_1/easy_1_exercise_1.rb
@@ -0,0 +1,16 @@
+=begin
+Write a method that takes two arguments, 
+a string and a positive integer, 
+and prints the string as many times as the integer indicates.
+=end
+
+
+def repeat(str, rpt)
+  loop do
+    rpt -= 1
+    puts str
+  break if rpt == 0
+  end
+end
+
+repeat("Hello", 3)
