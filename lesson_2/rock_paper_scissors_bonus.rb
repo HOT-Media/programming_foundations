c1d6a8e2578c479483731a6fe4251f1be24fe8e0
HOT-Media <jeremy@hotmediaglobalconnect.com>
2018-07-06 09:52:02 -0500

    Change winning_combinations hash keys to strings and remove .to_sym from 38.

--- lesson_2/rock_paper_scissors_bonus.rb
+++ lesson_2/rock_paper_scissors_bonus.rb
@@ -26,6 +26,24 @@ loop do # continue playing the game until someone wins 5
   Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
   puts
 
+winning_combinations =
+   {'rock'=>["scissors", "lizzard"],
+   'paper'=>["rock", "spock"],
+   'scissors'=>["paper", "lizzard"],
+   'lizzard'=>["spock", "paper"],
+   'spock'=>["scissors", "rock"]}
+
+if choice == computer_choice
+    puts "It's a tie, no point awarded for this round."
+  elsif winning_combinations.fetch(choice).include?(computer_choice) == true
+    puts 'You won that round!'
+    your_score += 1
+  else
+    puts 'The computer won that round!'
+    computer_score += 1
+  end
+  
+=begin
   winning_combinations =
    {:rock=>["scissors", "lizzard"],
    :paper=>["rock", "spock"],
@@ -35,15 +53,15 @@ loop do # continue playing the game until someone wins 5
 
   if choice == computer_choice
     puts "It's a tie, no point awarded for this round."
-  elsif 
-    winning_combinations.fetch(choice.to_sym).include?(computer_choice) == true
+  elsif winning_combinations.fetch(choice.to_sym).include?(computer_choice) == true
     puts 'You won that round!'
-     your_score += 1
+    your_score += 1
   else
     puts 'The computer won that round!'
-      computer_score += 1
+    computer_score += 1
   end
 
+=end
   puts "
 Your score is: #{your_score} The computer's score is: #{computer_score}"
   puts
@@ -55,5 +73,4 @@ Your score is: #{your_score} The computer's score is: #{computer_score}"
     puts 'The computer won the game!!'
     break
   end
-
 end # end the play again loop
