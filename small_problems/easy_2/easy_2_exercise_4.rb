c8f0dd8a1c20161cdb841a16fe69b1b7da0627d7
HOT-Media <jeremy@hotmediaglobalconnect.com>
2018-07-09 15:15:34 -0500

    add easy 2 exercies 4,5,6

--- small_problems/easy_2/easy_2_exercise_4.rb
+++ small_problems/easy_2/easy_2_exercise_4.rb
@@ -1,12 +1,14 @@
-puts "What is the bill?"
-bill = gets.chomp.to_f
+puts "What is your age?"
+age = gets.chomp.to_i
 
-puts "What is the tip percentage?"
-tip_percentage = gets.chomp.to_i
+puts "At what age would you like to retire?"
+retirement_age = gets.chomp.to_i
 
-tip = bill * tip_percentage.to_f * 0.01
-total_bill = bill + tip
-total_bill = format('%.2f', total_bill)
+current_year = Time.now.year
 
-puts "The tip is $#{tip.round(2)}"
-puts "The total is $#{total_bill}"
+years_to_work = retirement_age - age
+
+year_to_retire = 2018 + years_to_work
+
+puts "It's #{current_year}. You will retire in #{year_to_retire}"
+puts "You have only #{years_to_work} years of work to go!"
