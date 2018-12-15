647cbf9c6826d19fb9d76a4de8f984dab719a0b4
HOT-Media <jeremy@hotmediaglobalconnect.com>
2018-12-15 15:18:03 -0600

    Update 21 files

--- lesson_6_slightly_larger_programs/21_1_2_3_4_5_sub_process/calculate_total.md
+++ lesson_6_slightly_larger_programs/21_1_2_3_4_5_sub_process/calculate_total.md
@@ -81,7 +81,256 @@ def calculate_hand_values(hand)
 end
 
 
+*****************************************************************************************************
 
+downcard_hand = [["?", "?"], ["H", "K"]]
+=> [["?", "?"], ["H", "K"]]
+def dog x
+  puts x
+end  
+=> :dog
+dog((downcard_hand[1][1]))
+K
+=> n
+
+
+
+
+
+
+
+K Q J A or just value 
+no addition just assignment
+
+                                        |
+                                        V
+    downcard_hand = [["?", "?"], ["H", "K"]]
+    downcard_hand_value(        "K"         )
+    downcard_hand_value(downcard_hand[1][1])
+
+
+def downcard_hand_value(upcard_value)
+  value = if upcard_value == "A"
+            11
+          elsif upcard_value.to_i == 0
+            10
+          else
+            upcard_value.to_i
+          end
+end
+
+downcard_hand_value(downcard_hand[1][1])
+
+
+def calculate_face_values
+
+def calculate_integers 
+
+
+player_hand = [["D", "10"], ["H", "K"]]
+
+def calculate_hand_values(hand)
+  values = hand.map { |card_sub_array| card_sub_array[1] }
+=> ["10", "K"] # add these together
+
+  ["10", "K"]
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+  values.select { |card_value| card_value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+  sum
+end
+
+
+def extract_values(hand)
+  values = hand.map { |card_sub_array| card_sub_array[1] }
+end
+
+=> ["10", "K"] # add these together
+
+def convert_values_to_integers(values)
+    values.map! do |value|
+    if value == "A"
+      11
+    elsif value.to_i == 0
+      10
+    else
+      value.to_i
+    end
+  end
+end
+=> [10,10]
+
+.sum
+
+
+def extract_values(hand)
+  values = hand.map { |card_sub_array| card_sub_array[1] }
+end
+
+def convert_values_to_integers(values)
+    values.map! do |value|
+    if value == "A"
+      11
+    elsif value.to_i == 0
+      10
+    else
+      value.to_i
+    end
+  end
+end
+
+def calculate_hand_value(hand)
+  value_array = extract_values(hand)
+  integer_array = convert_values_to_integers(value_array)
+  integer_array.sum
+end
+
+
+player_hand = [["D", "10"], ["H", "K"]]
+
+calculate_hand_value(player_hand)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# good
+bar = if foo
+        1
+      else
+        2
+      end
+
+
+
+
+
+
+
+end
+
+
+
+
+
+extract Ace logic and calculation 
+
+
+
+Style/ConditionalAssignment:
+  Description: >-
+
+Use the return value of `if` and `case` statements for
+                 assignment to a variable and variable comparison instead
+                 of assigning that variable inside of each branch.
+
+EnforcedStyle: assign_to_condition
+
+# bad
+if foo
+  bar = 1
+else
+  bar = 2
+end
+
+case foo
+when 'a'
+  bar += 1
+else
+  bar += 2
+end
+
+if foo
+  some_method
+  bar = 1
+else
+  some_other_method
+  bar = 2
+end
+
+# good
+bar = if foo
+        1
+      else
+        2
+      end
+
+
+
+
+  if hand_value == "A"
+    sum += 11
+  elsif hand_value == "?"
+    sum += 0
+  elsif hand_value.to_i == 0
+    sum += 10
+  else
+    sum += hand_value.to_i
+  end
+  sum -= 10 if sum > 21
+  sum
+
+
+
+bar += case foo
+       when 'a'
+         1
+       else
+         2
+       end
+
+bar << if foo
+         some_method
+         1
+       else
+         some_other_method
+         2
+       end
+
+
+
+
+
+
+
+
+
+def calculate_hand_values(hand)
+  values = hand.map { |card_sub_array| card_sub_array[1] }
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+  values.select { |card_value| card_value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+  sum
+end
 
 
 
--- lesson_6_slightly_larger_programs/21_1_2_3_4_5_sub_process/diagnostics_with_pry.rb
+++ lesson_6_slightly_larger_programs/21_1_2_3_4_5_sub_process/diagnostics_with_pry.rb
@@ -3,7 +3,7 @@ require 'pry-byebug'
 
 RULES = <<TWENTY_ONE_RULES
 
-You start with a normal 52-card deck consisting of the 4 suits: 
+You start with a normal 52-card deck consisting of the 4 suits:
 (hearts, diamonds, clubs, and spades),and 13 values:
 (2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace)
 
@@ -11,35 +11,35 @@ The goal of Twenty-One is to try to get as close to 21 as possible,
 without going over. If you go over 21, it's a "bust" and you lose.
 
 
-Card values: Cards numberd 2 through 10 are worth their face value. The 
+Card values: Cards numberd 2 through 10 are worth their face value. The
 jack, queen, and king are each worth 10, and the ace can be worth 1
-or 11. The ace's value is determined each time a new card is drawn 
+or 11. The ace's value is determined each time a new card is drawn
 from the deck. For example, if the hand contains a 2, an ace, and a 5,
 then the total value of the hand is 18. In this case, the ace is worth
 11 because the sum of the hand (2 + 11 + 5) doesn't exceed 21.
 
-Game play: 
+Game play:
 A "hand" reffers to the cards you are currently holding in your hand.
-The game begins when the dealer deals a "hand" 
+The game begins when the dealer deals a "hand"
 The cards are dealt in this order:
-player - face up 
+player - face up
 dealer - face down, which is reffered to as the "downcard"
 player - face up
 dealer - face up
 
- 
+
 You are the player, and you go first.
-After you are dealt your first two cards, you will be prompted to 
-either "hit" or "stay". 
+After you are dealt your first two cards, you will be prompted to
+either "hit" or "stay".
 Type "hit" for another card.
-Type "stay" if you are satified with your "hand" and you do not want 
+Type "stay" if you are satified with your "hand" and you do not want
 another card.
 If the total exceeds 21 after a card has been dealt to you, then you
 "bust" and the dealer wins.
 
 
 After you "stay", the dealer will reveal their downcard.
-If the value of the dealers "hand" is less than 17, the dealer 
+If the value of the dealers "hand" is less than 17, the dealer
 "hits" until the value of their "hand" is greater than 17
 If the value of the dealers "hand" is now greater than 21, the dealer
 "busts"
@@ -48,7 +48,7 @@ of your "hand" is greater than the value of the dealers "hand"
 you win the "hand", otherwise the dealer wins the "hand".
 If the value of the dealers "hand" is now less than 21 and the value
 of your "hand" and the value of the dealers "hand" are equal, this is
-a tie which is reffered to as a "push", and no points are awarded for 
+a tie which is reffered to as a "push", and no points are awarded for
 that "hand".
 
 
@@ -114,15 +114,40 @@ UNSHUFFLED_DECK = [
                   ["C", "J"]
                  ]
 
+
+
+
+
+
+
+
+
+
 WINNING_SCORE = 5
-UNSHUFFLED_DECK.freeze
+FACE_CARDS = ["K", "Q", "J", "A"]
+SUITS = ["H", "D", "C", "S"]
+
+
+def construct_deck
+  cards = []
+  2.upto(10){|num| cards << num.to_s}
+  FACE_CARDS.each{|suit| cards << suit}
+  deck = cards.product(SUITS)
+  deck.map!{|card| card.reverse }
+end
+
+construct_deck
+
+
+#WINNING_SCORE = 5
+#UNSHUFFLED_DECK.freeze
 
 def clear_screen
   system('clear') || system('cls')
 end
 
 def welcome_message
-  msg =  <<WELCOME 
+  msg = <<WELCOME
 Welcome to 21.
 The winner of each "hand" will earn 1 point.
 The game is won when either you or the dealer win 5 hands.
@@ -131,15 +156,13 @@ WELCOME
 end
 
 def display_rules?
-  loop do 
+  loop do
     puts "Would you like to see the rules? yes or no "
     answer = gets.chomp.downcase
     if answer == "yes" || answer == "y"
       return true
-      break
     elsif answer == "no" || answer == "n"
       return false
-      break
     else puts "Valid answers are yes or no"
     end
   end
@@ -164,54 +187,78 @@ def dealers_downcard_hand(dealers_hand)
 end
 
 def display_cards_player_turn(player, dealer)
-  puts 
+  puts
   puts "Your hand:"
-  player.each{|card| puts "#{card}" }
+  player.each { |card| puts card.to_s }
   puts
   puts "Dealers hand"
-  puts "#{dealer[0]}"
-  puts "#{dealer[2]}"
+  puts dealer[0].to_s
+  puts dealer[2].to_s
+  # puts "#{dealer[0]}"
+  # puts "#{dealer[2]}"
   puts
 end
 
 def display_cards_dealer_turn(player, dealer)
   puts
   puts "Your hand:"
-  player.each{|card| puts "#{card}" }
+  player.each { |card| puts card.to_s }
   puts
   puts "Dealers hand"
-  dealer.each{|card| puts "#{card}" }
+  dealer.each { |card| puts card.to_s }
   puts
 end
 
 def display_downcard_message(player, dealer)
-  puts 
+  puts
   puts "Your hand:"
-  player.each{|card| puts "#{card}" }
+  player.each { |card| puts card.to_s }
   puts
   puts "The dealer is revealing their downcard"
-  puts "#{dealer[0]}"
-  puts "#{dealer[2]}"
+  puts dealer[0].to_s
+  puts dealer[2].to_s
   puts
 end
 
+
+
+def downcard_hand_value(hand)
+  hand_value = hand[1][1]
+  sum = 0
+  case
+  when hand_value == "A"
+    sum += 11
+  when hand_value == "?"
+    sum += 0
+  when hand_value.to_i == 0
+    sum += 10
+  else
+    sum += hand_value.to_i
+  end
+  sum 
+end
+
+
+
+
+
+
+
 def calculate_hand_values(hand)
-  values = hand.map{ |card_sub_array| card_sub_array[1] }
-    sum = 0
-    #binding.pry
-    values.each do |value|
-      if value == "A" # "ACE"
-        sum += 11
-      elsif value == "?"
-        sum += 0
-      elsif value.to_i == 0 # J, Q, K 
-        sum += 10 
-      else
-        sum += value.to_i
-      end
+  values = hand.map { |card_sub_array| card_sub_array[1] }
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+#    elsif value == "?"
+#      sum += 0
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
     end
-  # correct for Aces
-  values.select{ |card_value| card_value == "A" }.count.times do
+  end
+  values.select { |card_value| card_value == "A" }.count.times do
     sum -= 10 if sum > 21
   end
   sum
@@ -223,7 +270,7 @@ def display_hand_values(player, dealer)
 end
 
 def twenty_one?(hand)
-  hand == 21 
+  hand == 21
 end
 
 def over_seventeen?(hand)
@@ -253,22 +300,22 @@ def push?(players_hand_value, dealers_hand_value)
   players_hand_value == dealers_hand_value
 end
 
-def hit?
-  hit_question = <<HIT_OR_NOT
+HIT_QUESTION = <<HIT_OR_NOT
   "What do you want to do?"
   "Enter hit for another card"
   "Enter stay if you do not want another card"
 HIT_OR_NOT
-  #binding.pry
-  loop do 
-    puts hit_question
+
+def hit?
+  loop do
+    puts HIT_QUESTION
     answer = gets.chomp.downcase
     if answer == "hit" || answer == "h"
       return true
-      break
+      # break
     elsif answer == "stay" || answer == "s"
       return false
-      break
+      # break
     else
       puts "Valid responses are hit or stay"
     end
@@ -284,8 +331,7 @@ def display_dealing_to_dealer
   puts "The dealer is dealing themself another card"
 end
 
-def who_won_hand?(player,dealer)
-  #binding.pry
+def who_won_hand?(player, dealer)
   return "Push" if player == dealer
   return "Dealer" if player > 21
   return "Player" if dealer > 21
@@ -310,7 +356,7 @@ def add_one_point(hand_winner, dealer, player)
   end
 end
 
-def display_game_score(dealers_score,players_score)
+def display_game_score(dealers_score, players_score)
   puts "Your score: #{players_score.sum} Dealers score: #{dealers_score.sum}"
 end
 
@@ -319,25 +365,25 @@ def someone_won_game? player_score, dealer_score
 end
 
 def who_won_game? player_score, dealer_score
-  player_score.sum > dealer_score.sum ? "Player" : "Dealer" 
+  player_score.sum > dealer_score.sum ? "Player" : "Dealer"
 end
 
-def display_game_winner winner
-winner = "You" if winner == "Player" 
+def display_game_winner(winner)
+  winner = "You" if winner == "Player"
   puts "#{winner} won the game!"
 end
 
 def play_again?
   loop do
     puts "Would you like to play another game?, yes or no "
-    answer = gets.chomp.downcase 
+    answer = gets.chomp.downcase
     if answer == "yes" || answer == "y"
       return true
-      break
+      # break
     elsif answer == "no" || answer == "n"
       return false
-      break
-    else 
+      # break
+    else
       puts "Valid answers are yes or no"
     end
   end
@@ -346,14 +392,15 @@ end
 def enter_to_continue
   puts "Press enter to continue"
   gets
-end  
+end
 
 def enter_to_deal_cards
   puts "Press enter to deal the cards"
   gets
 end
 
-deck = UNSHUFFLED_DECK.shuffle
+#deck = construct_deck.shuffle
+# deck = UNSHUFFLED_DECK.shuffle
 player_score = []
 dealer_score = []
 player_busted = nil
@@ -365,9 +412,10 @@ enter_to_deal_cards
 loop do
   clear_screen
   loop do
-    display_game_score dealer_score,player_score
+    deck = construct_deck.shuffle
+    display_game_score dealer_score, player_score
     players_hand = []
-    dealers_hand = [ ["?", "?"]]
+    dealers_hand = [["?", "?"]]
     deal_to_player players_hand, deck
     deal_to_dealer dealers_hand, deck
     deal_to_player players_hand, deck
@@ -375,35 +423,37 @@ loop do
     downcard_hand = dealers_downcard_hand(dealers_hand)
     display_cards_player_turn(players_hand, dealers_hand)
     player_hand_value = calculate_hand_values(players_hand)
-    dealer_hand_value = calculate_hand_values(downcard_hand)
+    dealer_hand_value = downcard_hand_value(downcard_hand)
+    # dealer_hand_value = calculate_hand_values(downcard_hand)
+    # dealer_hand_value = dealer_downcard_hand_value(downcard_hand)
     display_hand_values(player_hand_value, dealer_hand_value)
 
-    loop do # players turn
+    loop do
       twenty_one = twenty_one?(player_hand_value)
       display_player_twenty_one if twenty_one == true
-      break if twenty_one == true 
+      break if twenty_one == true
       hit_player = hit?
       clear_screen
       deal_to_player(players_hand, deck) if hit_player == true
       player_hand_value = calculate_hand_values(players_hand)
-      display_game_score dealer_score,player_score
+      display_game_score dealer_score, player_score
       display_cards_player_turn(players_hand, dealers_hand)
       display_hand_values(player_hand_value, dealer_hand_value)
       player_busted = bust?(player_hand_value)
       display_player_bust if player_busted == true
       break if hit_player == false || player_busted == true
-    end # end players turn loop
+    end
 
     clear_screen
-    display_game_score dealer_score,player_score
-    display_downcard_message(players_hand, dealers_hand) # dealer revealing downcard
+    display_game_score dealer_score, player_score
+    display_downcard_message(players_hand, dealers_hand)
     display_hand_values(player_hand_value, dealer_hand_value)
     sleep 2
-    dealer_reveal_downcard(dealers_hand) # shift / delete downcard
+    dealer_reveal_downcard(dealers_hand)
 
-    loop do # dealers hand loop
+    loop do
       clear_screen
-      display_game_score dealer_score,player_score
+      display_game_score dealer_score, player_score
       display_cards_dealer_turn(players_hand, dealers_hand)
       dealer_hand_value = calculate_hand_values(dealers_hand)
       display_hand_values(player_hand_value, dealer_hand_value)
@@ -415,21 +465,21 @@ loop do
       display_dealing_to_dealer
       sleep 2
       deal_to_dealer(dealers_hand, deck) if over_seventeen?(dealer_hand_value) == false
-    end # end dealer hand
+    end
 
     hand_winner = who_won_hand? player_hand_value, dealer_hand_value
     display_hand_winner(hand_winner)
     add_one_point(hand_winner, dealer_score, player_score)
     enter_to_continue
     clear_screen
-    break if someone_won_game?(player_score, dealer_score)# break gameplay loop
-  end # end first to five loop
+    break if someone_won_game?(player_score, dealer_score)
+  end
   game_winner = who_won_game?(player_score, dealer_score)
-  display_game_score dealer_score,player_score
+  display_game_score dealer_score, player_score
   display_game_winner(game_winner)
   player_score.clear
-  dealer_score.clear 
-  play_another_game = play_again? # y true
+  dealer_score.clear
+  play_another_game = play_again?
   break if play_another_game == false
-end # end play again loop
+end
 puts "Thanks for playing"
